f66a5596a20e89150cbee43c1a33a023fafecdfe
+ <+>
2022-04-11 10:50:39 -0400

    commit tree 87628e170cc8e8dd73bd5aa1cbe0855463dc5fec

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Your code goes here
+document.addEventListener('DOMContentLoaded', function() {
+    let change = document.querySelector('p#text')
+    change.textContent = 'This is really cool!'
+    console.log('The DOM has loaded');
+})
+console.log('The DOM is loading')
